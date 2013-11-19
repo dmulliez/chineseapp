52cc88897ba4432b07c5b966875fe85fb27e380c
damienmulliez <damien@l3l.fr>
2013-11-19 12:15:21 +0100

    First Commit ChineseApp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chineseapp::Application.config.secret_token = 'c7f6ac17ada822e15b1e15bb7c2c1193452d09a05342d886ccf410dd26708dba073fac4d3cb32872f310e170af0cfe63cb1651ba2330c56c768307687cacbde6'
